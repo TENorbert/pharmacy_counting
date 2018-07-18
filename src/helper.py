0a282705772f74904576406eda589f1585067540
TENorbert <delsubdue@gmail.com>
2018-07-18 15:57:04 -0500

    compute results

--- src/helper.py
+++ src/helper.py
@@ -15,9 +15,15 @@ def command_line_parser():
 
 
 def get_data(filename):
+    """
+     reads drugs data from file and returns dict with drug_id as key
+     and the rest items as list of tuples.
+    :param filename:
+    :return: drug data as dict
+    """
     data = {}
     with open(filename, 'r') as fobj:
-        next(fobj) # skip first line!
+        next(fobj)  #skip first line!
         lines = fobj.readlines()
         for line in lines:
             id, last_name, first_name, drug_name, drug_cost = line.strip().split(',')
@@ -26,6 +32,172 @@ def get_data(filename):
     return data
 
 
+
+def get_data_lines(filename):
+    """
+    reads a single line from file avoiding loading all lines at once!
+    :param filename:
+    :return:
+    """
+    with open(filename, 'r') as fobj:
+        next(fobj)  #skip first line!
+        lines = fobj.readlines()
+    return lines
+
+
+
+def extract_drug_properties(lines):
+    """extract drug data from lines read"""
+    drug_name_list = []
+    drug_cost_list = []
+    drug_prescriber_list = []
+    drug_id_list = []  # Unique drug id
+    for line in lines:
+        line = line.strip().split(',')
+        if len(line) == 5:
+            drug_id = line[0]
+            prescriber_name = line[1] + " " + line[2]
+            drug_name = line[3]
+            drug_cost = line[4]
+            #get interested drug properties
+            drug_id_list.append(drug_id)
+            drug_name_list.append(drug_name)
+            drug_cost_list.append(drug_cost)
+            drug_prescriber_list.append(prescriber_name)
+            """
+                #For given drug_name:
+                   # check if drug_name_already exist: search(drug_name,drug_name_list)
+                      # Check if prescriber already exist: search(drug_name,
+                        # if prescriber already exists: increase cost and number_of_prescribers
+                        # else: Add new prescriber and cost to drug_data dict by drug_name
+                #drug_data[drug_name].add((unique_prescriber_name_list, unique_drug_cost_list))
+            """
+
+    return drug_name_list, drug_cost_list, drug_prescriber_list
+
+
+
+def compute_drug_outputs(drug_name_list, drug_cost_list, drug_prescriber_list,unique_prescriber_name, drug_costs):
+    """
+    Computes drug properties:
+    :param drug_name_list:
+    :param drug_cost_list:
+    :param drug_prescriber_list:
+    :param unique_prescriber_name:
+    :param drug_costs:
+    :return: Drug Name, Number of Prescribers, Total cost
+    """
+
+    '''
+    ## Get list of unique Prescriber for each drug
+    for item in drug_name_list:
+        print('Drug Name = %s' %item)
+
+    for item in drug_prescriber_list:
+        print('Drug Prescriber = %s' %item)
+
+    for item in drug_cost_list:
+        print('Drug Cost = %s' %item)
+    '''
+
+    for drug, prescriber in zip(drug_name_list, drug_prescriber_list):
+        print('Drug = %s & Drug Prescriber = %s' % (drug, prescriber))
+        if prescriber not in unique_prescriber_name:
+            unique_prescriber_name[drug] = set() ## makes prescriber unique
+        unique_prescriber_name[drug].add(prescriber)
+
+
+
+    ## Get list of drug cost for each drug
+    for drug, cost in zip(drug_name_list, drug_cost_list):
+        print('Drug = %s & Drug Cost = %s' % (drug,cost))
+        if cost not in drug_costs: ## do I really need this?
+            drug_costs[drug] = list() # list of costs
+        drug_costs[drug].append(cost)
+
+
+
+    #return drug_costs, unique_prescriber_name
+
+
+
+def print_drug_output(drug_costs, unique_drug_prescribers):
+    """
+        Takes drug_costs and uniqiue_drug_prescribers and computes:
+            -- Total drugs
+            -- Number of Unique Prescribers
+            -- Total cost of drug
+    """
+    drugs_from_drug_costs = [key for key in drug_costs.keys()]
+    drugs_from_unique_drug_prescribers = [key for key in unique_drug_prescribers.keys()]
+
+    print("length of drug cost = {} & length of unique prescribers = {}\n"\
+        .format(len(drugs_from_drug_costs),len(drugs_from_unique_drug_prescribers)))
+
+    print("Drug Costs-----")
+    for key, value in drug_costs.items():
+        print("Key = {}, Value = {}".format(key, value))
+    print("\n")
+
+    print("Drug Prescribers------")
+    for key, value in unique_drug_prescribers.items():
+        print("Key = {}, Value = {}".format(key, value))
+
+    print("\n")
+
+    if len(drugs_from_drug_costs) == len(drugs_from_unique_drug_prescribers):
+        for drug in drug_costs.keys():
+            if drug in unique_drug_prescribers.keys():
+                total_drug_cost = get_sum(drug_costs[drug])
+                prescriber_count = len(unique_drug_prescribers[drug])
+
+                print("Drug Name = {}, Total Cost = {} , Number of Unique Prescribers = {}".format(drug, total_drug_cost, prescriber_count))
+    else:
+        print("Houston we have a problem! Not same number of drugs on cost and prescribers dicts!")
+
+
+
+def  get_sum(cost_list):
+    """
+    sums up items in a list
+    :param cost_list:
+    :return: sum_total
+    """
+    total_cost = 0
+    for cost in cost_list:
+        total_cost += float(cost)
+
+    return total_cost
+
+
+#######
+''''
+### Brain Storming ###
+ ** Each Drug_name has MANY
+        1) prescribers
+        2) drug costs
+ ** For each Drug:
+        we want:
+            --->Number  of prescribers == len(prescribers)
+            ---> Total Cost = sum(drug_costs)
+## Inputs
+    --> drug_id
+    --> prescriber_name(first_name, last_name)
+    --> drug_name
+    --> drug_cost
+
+## Outputs: 
+    --> drug name
+    --> Number of UNIQUE prescribers
+    --> Total drug cost
+    
+## methods
+   ---> get_unique_drug_name()
+   ---> count_number_of_drugs()
+   ---> compute_total_drug_cost()
+##
+   
+'''
 
 
 
--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -3,7 +3,9 @@
 
 import os, sys, re
 
-from helper import *
+from helper import command_line_parser, \
+    get_data_lines,extract_drug_properties,\
+    compute_drug_outputs,print_drug_output
 
 
 def main():
@@ -14,18 +16,29 @@ def main():
         input_file = readfiles['input']
         output_file = readfiles['output']
 
+        unique_prescriber_name = {}  # unique prescriber of a drug type
+        drug_costs = {}  # list of costs for each drug
 
-        data = read_input_data(input_file)
-
+        """
+        #data = read_input_data(input_file)
         print_lines(data)
-
         #write_output_data(data, output_file)
-
         data_dict = get_data(input_file)
         for key in data_dict.keys():
             print(str(key) + " : " + str(data_dict[key]))
+        """
+        lines = get_data_lines(input_file)
 
+        drug_name_list, drug_cost_list, drug_prescriber_list = extract_drug_properties(lines)
 
+        print("drugs_name length = {}, costs_length= {}, prescriber_length = {}\n" \
+              .format(len(drug_name_list), len(drug_cost_list), len(drug_prescriber_list)))
+
+        compute_drug_outputs(drug_name_list, drug_cost_list,\
+                             drug_prescriber_list,\
+                             unique_prescriber_name, drug_costs)
+
+        print_drug_output(drug_costs, unique_prescriber_name)
 
     except IOError:
         print('main cannot run!')
